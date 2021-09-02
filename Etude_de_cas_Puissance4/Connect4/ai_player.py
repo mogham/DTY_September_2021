9a4ebe7dca1473cc3fb22ab1ccde17a5994911e7
Mohamed Khairallah Gharbi <mohamed-khairallah.gharbi@student-cs.fr>
2021-09-02 18:20:52 +0200

    Correcting AI agent

--- Etude_de_cas_Puissance4/Connect4/ai_player.py
+++ Etude_de_cas_Puissance4/Connect4/ai_player.py
@@ -1,4 +1,10 @@
 from player import Player
+from game import Game
+import math
+import random
+import copy
+
+import logging
 
 class AIPlayer(Player):
     """This player should implement a heuristic along with a min-max and alpha
@@ -7,71 +13,122 @@ class AIPlayer(Player):
     def __init__(self):
         self.name = "VenusAI"
 
-    
     def getColumn(self, board):
-         # TODO(student): implement this!
-        return 0
-
-    def miniMax(self):
-        return 0
+        return self.alphaBetaMinMax(board, 4, -math.inf, math.inf, True)[0]     
     
-    def alphaBeta(self):
-        return 0
+    def alphaBetaMinMax(self, board, depth, alpha, beta, maximizing):
+        possibleColumns = board.getPossibleColumns()
+        # Base Case
+        if self.isTerminal(board): 
+            if self.checkWinningSituation(board, self.color):
+                return (None, math.inf)
+            elif self.checkWinningSituation(board, self.color*(-1)):
+                return (None, -math.inf)
+            else: # Draw
+                return (None, 0)
+        if depth == 0:
+            return (None, self.scoring(board))
+        if maximizing:
+            value = -math.inf
+            columnToPick = random.choice(possibleColumns)
+            for column in possibleColumns:
+                boardCopy = copy.deepcopy(board)
+                boardCopy.play(self.color, column)
+                score = self.alphaBetaMinMax(boardCopy, depth-1, alpha, beta, False)[1]
+                if score > value:
+                    value = score
+                    columnToPick = column
+                alpha = max(alpha, value)
+                if alpha >= beta:
+                    break
+            return columnToPick, value
+        else: #Min
+            value = math.inf
+            columnToPick = random.choice(possibleColumns)
+            for column in possibleColumns:
+                boardCopy = copy.deepcopy(board)
+                boardCopy.play(self.color*(-1), column)
+                score = self.alphaBetaMinMax(boardCopy, depth-1, alpha, beta, True)[1]
+                if score < value:
+                    value = score
+                    columnToPick = column
+                beta = min(beta, value)
+                if alpha >= beta:
+                    break
+            return columnToPick, value
     
 
     def cost(self, subArray, player):
         score = 0
         if subArray.count(player) == 4:
-            score += 16
+            score += 10000
         elif subArray.count(player) == 3 and subArray.count(0) == 1: 
-            score += 8
+            score += 5
         elif subArray.count(player) == 2 and subArray.count(0) == 2:
             score += 4
         if subArray.count(player*(-1)) == 3 and subArray.count(0) == 1:
-            score += 8
+            score -= 5
+        elif subArray.count(player*(-1)) == 2 and subArray.count(0) == 2:
+            score -= 4
         return score
 
     def scoring(self, board):
-        score = 0 
+        score = 0
         # Center column 
-        middleColumnArray = [int(i) for i in list(board[:, 3])]
+        middleColumnArray = [int(i) for i in list(board[3])]
         middleCount = middleColumnArray.count(self.color)
-        score += middleCount*4
+        score += middleCount*2
         #Vertical
         for column in range(5):
-            columnArray = [int(i) for i in list(board[:,column])]
+            columnArray = [int(i) for i in list(board[column])]
             for row in range(3):
                 subRow = columnArray[row: row+4]
                 score += self.cost(subRow, self.color)
         #Horizontal
         for row in range(6):
-            rowArray = [int(i) for i in list(board[row,:])]
+            rowArray = []
+            for i in range(5): 
+                rowArray.append(board[i][row])
             for column in range(2):
                 subColumn = rowArray[column: column+4]
                 score += self.cost(subColumn, self.color)
-        # Diagonal positive 
+        # Diagonal positive
         for row in range(3):
             for column in range(2):
-                sub = [board[row+i][column+i] for i in range(4)]
+                sub = [board[column+i][row+i] for i in range(4)]
                 score += self.cost(sub, self.color)
-        # Diagonal negative 
+        # Diagonal negative
         for row in range(3):
             for column in range(2):
-                sub = [board[row+3-i][column+i] for i in range(4)]
+                sub = [board[column+i][row+3-i] for i in range(4)]
                 score += self.cost(sub, self.color)
         return score
 
-    def isTerminal(self, board, game):
-        possibleColumns = board.getPossibleColumns()
-        if possibleColumns == 0: 
-            return True
-        else:
-            result = False 
-            for possibleColumn in range(possibleColumns):
-                for possibleRow in range(board.getHeight(possibleColumn), 5):
-                    possiblePosition = [possibleColumn, possibleRow]
-                    if (game.getWinner(possiblePosition) == None or 
-                        game.getWinner(possiblePosition) == True or 
-                        game.getWinner(possiblePosition) == False):
-                        result = True
-            return result                             
+    def isTerminal(self, board):
+        return (len(board.getPossibleColumns()) == 0 or
+            self.checkWinningSituation(board, self.color) or
+            self.checkWinningSituation(board, self.color*(-1)))
+
+    def checkWinningSituation(self, board, color):
+        # Check Horizontal
+        for column in range(4):
+                for row in range(6):
+                    if board[column][row] == color and board[column+1][row] == color and board[column+2][row] == color and board[column+3][row] == color:
+                        return True
+        # Check positive Diagonal
+        for column in range(4):
+            for row in range(3):
+                if board[column][row] == color and board[column+1][row+1] == color and board[column+2][row+2] == color and board[column+3][row+3] == color:
+                    return True
+        # Check negative Diagonal
+        for column in range(4):
+            for row in range(3, 6):
+                if board[column][row] == color and board[column+1][row-1] == color and board[column+2][row-2] == color and board[column+3][row-3] == color:
+                    return True
+        # Check Vertical
+        for column in range(7):
+                for row in range(3):
+                    if board[column][row] == color and board[column][row+1] == color and board[column][row+2] == color and board[column][row+3] == color:
+                        return True
+
+    
--- Etude_de_cas_Puissance4/Connect4/run_ui.py
+++ Etude_de_cas_Puissance4/Connect4/run_ui.py
@@ -1,6 +1,7 @@
 import argparse
 
 from player import HumanPlayer, RandomPlayer
+from ai_player import AIPlayer
 from ui_game import UIGame
 
 
@@ -10,9 +11,9 @@ if __name__ == '__main__':
     parser.add_argument('--p2', default='player 2')
     args = parser.parse_args()
 
-    player1 = RandomPlayer()
+    player1 = HumanPlayer()
     player1.name = args.p1
-    player2 = HumanPlayer()
+    player2 = AIPlayer()
     player2.name = args.p2
 
     game = UIGame(player1, player2)
